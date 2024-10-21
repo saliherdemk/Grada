3467420376f3f834969710bc5e9dc7d00a7f9507
saliherdemk <saliherdem_kaymak@hotmail.com>
2024-10-21 17:50:22 +0300

    convertion try2

--- js/HandWritten/DigitInputGrid.js
+++ js/HandWritten/DigitInputGrid.js
@@ -3,28 +3,29 @@ class DigitInputGrid extends Draggable {
     super(x, y);
     this.gridSize = 28;
     this.cellSize = 10;
-    this.brushSize = 5;
-
     const size = this.gridSize * this.cellSize;
     this.setDimensions(size, size);
 
-    this.drawings = [];
-    this.drawingCanvas = createGraphics(size, size);
-    this.drawingCanvas.stroke(0);
-    this.drawingCanvas.strokeWeight(this.brushSize);
-
-    this.processedCanvas = createGraphics(this.gridSize, this.gridSize);
-
-    this.boundaries = { left: size, right: 0, top: size, bottom: 0 };
+    this.setupCanvases(size);
     this.clear();
     this.getData();
+
+    this.bounds = [Infinity, Infinity, -Infinity, -Infinity];
+    this.values = Array({ length: 784 }).fill(0);
+  }
+
+  setupCanvases(size) {
+    this.drawingCanvas = createGraphics(size, size);
+    this.drawingCanvas.stroke(255);
+    this.drawingCanvas.strokeWeight(10);
+
+    this.centeredCanvas = createGraphics(size, size);
+    this.normalizedCanvas = createGraphics(this.gridSize, this.gridSize);
   }
 
   clear() {
-    this.drawings = [];
-    this.drawingCanvas.clear();
-    this.drawingCanvas.background(255);
-    this.boundaries = { left: this.w, right: 0, top: this.h, bottom: 0 };
+    this.drawingCanvas.background(0);
+    this.bounds = [Infinity, Infinity, -Infinity, -Infinity];
   }
 
   setCoordinates(x, y) {
@@ -40,20 +41,85 @@ class DigitInputGrid extends Draggable {
     const { mouseX, mouseY, pmouseX, pmouseY } = getCurrentMouseCoordinates();
     const { x, y } = iManager.getAbsoluteCoordinates(mouseX, mouseY);
     const { x: px, y: py } = iManager.getAbsoluteCoordinates(pmouseX, pmouseY);
-
     const clamped = this.getClampedCoordinates(x, y, px, py);
-    this.drawings.push(clamped);
-
-    this.updateBoundaries(clamped);
 
     this.drawingCanvas.line(clamped.x1, clamped.y1, clamped.x2, clamped.y2);
+    this.startPipeline();
   }
 
-  updateBoundaries({ x1, y1, x2, y2 }) {
-    this.boundaries.left = Math.min(this.boundaries.left, x1, x2);
-    this.boundaries.right = Math.max(this.boundaries.right, x1, x2);
-    this.boundaries.top = Math.min(this.boundaries.top, y1, y2);
-    this.boundaries.bottom = Math.max(this.boundaries.bottom, y1, y2);
+  startPipeline() {
+    this.updateBounds();
+    this.centeredBoxTask();
+  }
+
+  updateBounds() {
+    this.drawingCanvas.loadPixels();
+    const p = this.drawingCanvas.pixels;
+    const w = this.drawingCanvas.width;
+
+    for (let i = 0; i < w * w; i++) {
+      const j = i * 4;
+      if (p[j] > 0 || p[j + 1] > 0 || p[j + 2] > 0) {
+        const x = i % w;
+        const y = Math.floor(i / w);
+        this.bounds = [
+          Math.min(x, this.bounds[0]),
+          Math.min(y, this.bounds[1]),
+          Math.max(x, this.bounds[2]),
+          Math.max(y, this.bounds[3]),
+        ];
+      }
+    }
+
+    this.bounds[2] -= this.bounds[0];
+    this.bounds[3] -= this.bounds[1];
+  }
+
+  getCroppedAndCentered() {
+    const [boundW, boundH] = [this.bounds[2], this.bounds[3]];
+    const scalingFactor = 200 / Math.max(boundW, boundH);
+    const croppedW = boundW * scalingFactor;
+    const croppedH = boundH * scalingFactor;
+    const [sX, sY] = [this.bounds[0], this.bounds[1]];
+    const centerX = this.centeredCanvas.width / 2 - croppedW / 2;
+    const centerY = this.centeredCanvas.height / 2 - croppedH / 2;
+
+    return [sX, sY, boundW, boundH, centerX, centerY, croppedW, croppedH];
+  }
+
+  centeredBoxTask() {
+    const centeredCanvas = this.centeredCanvas;
+    centeredCanvas.clear();
+    centeredCanvas.background(0);
+    centeredCanvas.copy(this.drawingCanvas, ...this.getCroppedAndCentered());
+
+    this.normalizedTask();
+  }
+
+  normalizedTask() {
+    const cc = this.centeredCanvas;
+    const nc = this.normalizedCanvas;
+    nc.clear();
+    nc.copy(cc, 0, 0, cc.width, cc.height, 0, 0, nc.width, nc.height);
+    this.setValues();
+  }
+
+  setValues() {
+    const pc = this.normalizedCanvas;
+    const gs = this.gridSize;
+    pc.loadPixels();
+    let pixelData = [];
+    for (let j = 0; j < gs; j++) {
+      for (let i = 0; i < gs; i++) {
+        let index = (i + j * gs) * 4;
+        let r = pc.pixels[index];
+        let g = pc.pixels[index + 1];
+        let b = pc.pixels[index + 2];
+        let brightness = (r + g + b) / 3;
+        pixelData.push(brightness / 255);
+      }
+    }
+    this.values = pixelData;
   }
 
   getClampedCoordinates(x, y, px, py) {
@@ -66,93 +132,13 @@ class DigitInputGrid extends Draggable {
     };
   }
 
-  showGrid() {
-    const commands = [{ func: "stroke", args: [0, 50] }];
-    const offset = this.gridSize * this.cellSize;
-
-    for (let i = 0; i <= this.gridSize; i++) {
-      const pos = i * this.cellSize;
-      commands.push(
-        {
-          func: "line",
-          args: [this.x + pos, this.y, this.x + pos, this.y + offset],
-        },
-        {
-          func: "line",
-          args: [this.x, this.y + pos, this.x + offset, this.y + pos],
-        },
-      );
-    }
-
-    executeDrawingCommands(commands);
-  }
-
-  showDrawings() {
+  showDrawingCanvas() {
     executeDrawingCommands([
-      { func: "image", args: [this.drawingCanvas, this.x, this.y] },
-      { func: "noFill", args: [] },
-      { func: "strokeWeight", args: [3] },
+      { func: "strokeWeight", args: [5] },
+      { func: "stroke", args: [255, 0, 0] },
       { func: "rect", args: [this.x, this.y, this.w, this.h] },
+      { func: "image", args: [this.drawingCanvas, this.x, this.y] },
     ]);
-    this.showGrid();
-  }
-
-  draw() {
-    this.showDrawings();
-    this.showProcessedCanvas();
-    this.setValues();
-    this.showProcessed();
-    this.showTestData();
-  }
-
-  calculateBoundaries() {
-    const brushSize = this.brushSize;
-    const dc = this.drawingCanvas;
-    const left = Math.max(this.boundaries.left - brushSize, 0);
-    const right = Math.min(this.boundaries.right + brushSize, dc.width);
-    const top = Math.max(this.boundaries.top - brushSize, 0);
-    const bottom = Math.min(this.boundaries.bottom + brushSize, dc.height);
-
-    return { left, right, top, bottom };
-  }
-
-  cropAndNormalizeImage(croppedWidth, croppedHeight, left, top) {
-    const dc = this.drawingCanvas;
-    const pc = this.processedCanvas;
-
-    const cropped = dc.get(left, top, croppedWidth, croppedHeight);
-
-    pc.clear();
-    pc.background(0);
-
-    pc.image(cropped, 0, 0, 28, 28);
-  }
-
-  showProcessedCanvas() {
-    const { left, right, top, bottom } = this.calculateBoundaries();
-    const croppedWidth = Math.max(right - left, 1);
-    const croppedHeight = Math.max(bottom - top, 1);
-
-    this.cropAndNormalizeImage(croppedWidth, croppedHeight, left, top);
-  }
-
-  setValues() {
-    const pc = this.processedCanvas;
-    const gs = this.gridSize;
-
-    pc.loadPixels();
-    let pixelData = [];
-    for (let i = 0; i < gs; i++) {
-      for (let j = 0; j < gs; j++) {
-        let index = (i + j * gs) * 4;
-        let r = pc.pixels[index];
-        let g = pc.pixels[index + 1];
-        let b = pc.pixels[index + 2];
-        let brightness = (r + g + b) / 3;
-        pixelData.push((255 - brightness) / 255);
-      }
-    }
-    this.values = pixelData;
   }
 
   async getData() {
@@ -160,47 +146,48 @@ class DigitInputGrid extends Draggable {
       const module = await import(`../../../data.js`);
       this.testData = module.default;
     } catch (error) {
-      console.error("Error loading yData:", error);
+      console.error("Error loading test data:", error);
     }
   }
 
   showTestData() {
     const x = this.x + this.w + 25;
     const y = this.y;
-
-    const commands = [{ func: "noStroke", args: [] }];
+    const commands = [
+      { func: "noStroke", args: [] },
+      { func: "text", args: ["example from mnist", x, y - 10] },
+    ];
 
     this.testData?.[0].forEach((b, i) => {
       const j = Math.floor(i / this.gridSize);
       commands.push(
         { func: "fill", args: [b * 255] },
-        {
-          func: "square",
-          args: [x + (i % this.gridSize) * 3, y + j * 3, 3],
-        },
+        { func: "square", args: [x + (i % this.gridSize) * 3, y + j * 3, 3] },
       );
     });
 
     executeDrawingCommands(commands);
   }
 
-  showProcessed(values = this.values) {
+  showValues() {
     const x = this.x + this.w + 25;
-    const y = this.y + (this.h - this.gridSize * 3) / 2;
-
+    const y = this.y + 100;
     const commands = [{ func: "noStroke", args: [] }];
 
-    values.forEach((b, i) => {
+    this.values.forEach((b, i) => {
       const j = Math.floor(i / this.gridSize);
       commands.push(
         { func: "fill", args: [b * 255] },
-        {
-          func: "square",
-          args: [x + j * 3, y + (i % this.gridSize) * 3, 3],
-        },
+        { func: "square", args: [x + (i % this.gridSize) * 3, y + j * 3, 3] },
       );
     });
 
     executeDrawingCommands(commands);
+  }
+
+  draw() {
+    this.showDrawingCanvas();
+    this.showTestData();
+    this.showValues();
   }
 }
